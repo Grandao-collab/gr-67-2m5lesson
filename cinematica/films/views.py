7bdb3244b6699eeceda420906377785825089adf
Grandao-collab <arystanbekovtilek6@gmail.com>
2026-09-04 19:06:55 +0600

    update

--- cinematica/films/views.py
+++ cinematica/films/views.py
@@ -16,11 +16,31 @@ def film_detail_api_view(request, id):
     return Response(data=data)
 
 
-@api_view(['GET'])
-def film_list_api_view(request):
-    # step 1: collect films (queryset)
-    films = Film.objects.select_related('director').prefetch_related('genres', 'Review').all()
-    # step 2: reformat queryset to list of dictionaries (serializer)
-    list_ = FilmListSerializer(films, many=True).data
-    # step 3: return response
-    return Response(data=list_)
+@api_view(['GET', 'POST'])
+def film_list_create_api_view(request):
+    if request.method == 'GET':
+        # step 1: collect films (queryset)
+        films = Film.objects.select_related('director').prefetch_related('genres', 'Review').all()
+            # step 2: reformat queryset to list of dictionaries (serializer)
+        list_ = FilmListSerializer(films, many=True).data
+            # step 3: return response
+        return Response(data=list_)
+    elif request.method == 'POST':
+        # step 1: receive data
+        title = request.data.get('title')
+        text = request.data.get('text')
+        release_year = request.data.get('release_year')
+        rating = request.data.get('rating')
+        is_hit = request.data.get('is_hit')
+        print(title, text, release_year, rating, is_hit)
+        # step 2: create film
+        film = Film.objects.create(
+            title=title,
+            text=text,
+            release_year=release_year,
+            rating=rating,
+            is_hit=is_hit
+        )
+        # step 3: return response
+        return Response(status=status.HTTP_201_CREATED, 
+                        data=FilmDetailSerializer(film).data)
